72ba72825eea1b91e5e0b5fdd90d178801b4fbc8
Péter Kalicz <kaliczp@gmail.com>
2019-01-23 13:07:14 +0100

    new data

--- ogimet.R
+++ ogimet.R
@@ -1,7 +1,10 @@
+teszt <- read.table("ET1812.txt", skip=24, na.strings=c("Tr","----"))
 teszt <- read.table("ET1901.txt", skip=24, na.strings=c("Tr","----"))
+
 adat <- teszt[,1:16]
 names(adat) <- c("Date","tmax","tmin","tavg","td","hr%","winddir","windint","windgust","airpress","Prec","TotOct","LowOct","sun","vis","snow")
 
+teljes <- adat
 sum(adat$Prec, na.rm=TRUE)
 
 ## Save to Excel compat.
