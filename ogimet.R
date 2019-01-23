4bae98e05583c8040e8154d481595ac752ddb81d
Péter Kalicz <kaliczp@gmail.com>
2019-01-23 12:57:01 +0100

    nodata handling

--- ogimet.R
+++ ogimet.R
@@ -1,3 +1,8 @@
-teszt <- read.table("ET1901.txt", skip=24)
+teszt <- read.table("ET1901.txt", skip=24, na.strings=c("Tr","----"))
 adat <- teszt[,1:16]
 names(adat) <- c("Date","tmax","tmin","tavg","td","hr%","winddir","windint","windgust","airpress","Prec","TotOct","LowOct","sun","vis","snow")
+
+sum(adat$Prec)
+
+## Save to Excel compat.
+write.csv2(adat,"adatteszt.csv")
