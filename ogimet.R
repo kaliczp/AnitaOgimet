88d51176f7544e06c395e06aba487c89c791a2bf
Péter Kalicz <kaliczp@gmail.com>
2019-01-23 21:54:07 +0100

    Read all files

--- ogimet.R
+++ ogimet.R
@@ -1,14 +1,21 @@
-teszt <- read.table("ET1812.txt", skip=24, na.strings=c("Tr","----","---"))
-teszt <- read.table("ET1901.txt", skip=24, na.strings=c("Tr","----","---"))
+filenames <- dir(patt="ET")
 
+i <- 1
+teszt <- read.table(filenames[i], skip=24, na.strings=c("Tr","----","---"))
 adat <- teszt[,1:16]
 names(adat) <- c("Date","tmax","tmin","tavg","td","hr%","winddir","windint","windgust","airpress","Prec","TotOct","LowOct","sun","vis","snow")
 
-teljes <- rbind(adat,teljes)
 teljes <- adat
 
+for(i in 2:length(filenames)) {
+    print(i)
+    teszt <- read.table(filenames[i], skip=24, na.strings=c("Tr","----","---"))
+    adat <- teszt[,1:16]
+    names(adat) <- c("Date","tmax","tmin","tavg","td","hr%","winddir","windint","windgust","airpress","Prec","TotOct","LowOct","sun","vis","snow")
+    teljes <- rbind(teljes,adat)
+}
 
 sum(teljes$Prec, na.rm=TRUE)
 
 ## Save to Excel compat.
-write.csv2(adat,"adatteszt.csv")
+write.csv2(teljes,"teljes.csv")
