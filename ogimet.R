b3745696f045b2652a16bd12cd775c8ba4100c45
Péter Kalicz <kaliczp@gmail.com>
2019-01-23 12:38:22 +0100

    col names

--- ogimet.R
+++ ogimet.R
@@ -1,2 +1,3 @@
 teszt <- read.table("ET1901.txt", skip=24)
 adat <- teszt[,1:16]
+names(adat) <- c("Date","tmax","tmin","tavg","td","hr%","winddir","windint","windgust","airpress","Prec","TotOct","LowOct","sun","vis","snow")
