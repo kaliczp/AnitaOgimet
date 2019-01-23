411cc000f0bbbddb50dc282df490f8df4f990486
Péter Kalicz <kaliczp@gmail.com>
2019-01-23 13:21:16 +0100

    new data connected

--- ogimet.R
+++ ogimet.R
@@ -1,11 +1,14 @@
-teszt <- read.table("ET1812.txt", skip=24, na.strings=c("Tr","----"))
-teszt <- read.table("ET1901.txt", skip=24, na.strings=c("Tr","----"))
+teszt <- read.table("ET1812.txt", skip=24, na.strings=c("Tr","----","---"))
+teszt <- read.table("ET1901.txt", skip=24, na.strings=c("Tr","----","---"))
 
 adat <- teszt[,1:16]
 names(adat) <- c("Date","tmax","tmin","tavg","td","hr%","winddir","windint","windgust","airpress","Prec","TotOct","LowOct","sun","vis","snow")
 
+teljes <- rbind(adat,teljes)
 teljes <- adat
-sum(adat$Prec, na.rm=TRUE)
+
+
+sum(teljes$Prec, na.rm=TRUE)
 
 ## Save to Excel compat.
 write.csv2(adat,"adatteszt.csv")
