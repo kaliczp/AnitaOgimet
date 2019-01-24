db4d4dd016f744bd5a5827a6a53cb91fe97c35ed
Péter Kalicz <kaliczp@gmail.com>
2019-01-24 16:06:45 +0100

    Data column added to MODIS et-s

--- modis.R
+++ modis.R
@@ -10,7 +10,7 @@ ncvar_get(teszt, "lon")
 ## Idő
 ncvar_get(teszt, "time")
 ## Értelmezhető idő
-as.Date("2000-01-01") + ncvar_get(teszt, "time")
+MODISdate <- as.Date("2000-01-01") + ncvar_get(teszt, "time")
 
 ##
 ncvar_get(teszt, "ET_500m")
@@ -21,14 +21,14 @@ H2 <- ncvar_get(teszt, "ET_500m", c(18,22,1), c(1,1,47))
 H3 <- ncvar_get(teszt, "ET_500m", c(19,21,1), c(1,1,47))
 H4 <- ncvar_get(teszt, "ET_500m", c(18,21,1), c(1,1,47))
 
-ETH <- data.frame(H1,H2,H3,H4)
+ETH <- data.frame(Date = MODISdate, H1, H2, H3, H4)
 
 PETH1 <- ncvar_get(teszt, "PET_500m", c(19,22,1), c(1,1,47))
 PETH2 <- ncvar_get(teszt, "PET_500m", c(18,22,1), c(1,1,47))
 PETH3 <- ncvar_get(teszt, "PET_500m", c(19,21,1), c(1,1,47))
 PETH4 <- ncvar_get(teszt, "PET_500m", c(18,21,1), c(1,1,47))
 
-PETH <- data.frame(PETH1,PETH2,PETH3,PETH4)
+PETH <- data.frame(Date = MODISdate, PETH1,PETH2,PETH3,PETH4)
 
 write.csv2(ETH, "eth.csv", row.names = FALSE)
 write.csv2(PETH, "peth.csv", row.names = FALSE)
